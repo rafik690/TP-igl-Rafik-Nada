99ebda87beeea2d3b5ac03c5c4f06759035274d6
nadaRaihan <fn_guennoun@esi.dz>
2017-10-13 22:19:16 +0100

    Exception & vector class

--- src/DifferentSizeException.java
+++ src/DifferentSizeException.java
@@ -1,5 +1,9 @@
 /**
  * Created by NADA on 13/10/2017.
  */
+
+/**
+ * Classe de l'exception générer si les tailles de deux vecteurs donnés sont différentes
+ */
 public class DifferentSizeException extends Exception {
 }
