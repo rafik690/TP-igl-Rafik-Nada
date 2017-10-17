2d62f375995c6014d2a0d43db645c97f091fb410
rafik690 <fr_drissi@esi.dz>
2017-10-17 16:55:08 +0100

    merged the tests, removed the main code, tested

--- Testing/VectorHelperTest.java
+++ Testing/VectorHelperTest.java
@@ -10,14 +10,44 @@ public class VectorHelperTest {
 
     @Test
     public void sumTwoVectors() {
+        Vector<Integer> vect1 = new Vector<Integer>(3);
+        Vector<Integer> vect2 = new Vector<Integer>(3);
+        Vector<Integer> vect3 = new Vector<Integer>(3);
+        vect1.add(0, 10);
+        vect1.add(1, 20);
+        vect1.add(2, 30);
+        vect2.add(0, 10);
+        vect2.add(1, 20);
+        vect2.add(2, 30);
+        VectorHelper vectorHelper = new VectorHelper();
+        try{
+        vect3 = vectorHelper.sumTwoVectors(vect1,vect2);}catch(Exception e){}
+        int a = 40;
+        assertEquals(a,(int)vect3.get(1));
     }
 
     @Test
     public void reverseVector() {
+        Vector<Integer> vect = new Vector<Integer>(3);
+        vect.add(0, 10);
+        vect.add(1, 20);
+        vect.add(2, 30);
+        VectorHelper vectorHelper = new VectorHelper();
+        vectorHelper.reverseVector(vect);
+        int a = 30;
+        assertEquals(a,(int)vect.get(0));
     }
 
     @Test
     public void squareVector() {
+        VectorHelper vectorHelper = new VectorHelper();
+        Vector<Integer> vect = new Vector<Integer>(3);
+        vect.add(0, 10);
+        vect.add(1, 20);
+        vect.add(2, 30);
+        vectorHelper.squareVector(vect);
+        int a = 900;
+        assertEquals(a,(int)vect.get(2));
 
     }
 
--- src/Main.java
+++ src/Main.java
@@ -5,41 +5,5 @@ import java.util.Vector;
 public class Main {
 
     public static void main(String[] args) {
-    /*
-        VectorHelper vi = new VectorHelper();
-        Vector<Integer> v = new Vector<Integer>();
-        for (int i=10;i>-10;i--){
-            v.add(i*2);
-        }
-        Vector<Integer> v2 = vi.trierVect(v);
-        for (int i=0;i<v2.capacity();i++){
-            System.out.print("  "+v2.get(i)+"  ");
-        }
-        System.out.println();
-        for (int i=0;i<v.capacity();i++){
-            System.out.print("  "+v.get(i)+"  ");
-        }
-*/
-        VectorHelper vectorHelper = new VectorHelper();
-        Vector v13= new Vector(100);
-        for(int i=0;i<=98;i++){
-            v13.add(i,-i);
-        }
-        System.out.println();
-        for(int i=0;i<=98;i++){
-            System.out.print("   "+v13.get(i));
-        }
-        System.out.println();
-        System.out.println("    numero 3");
-        v13.add(10,50);
-        for(int i=0;i<=98;i++){
-            System.out.print("   "+v13.get(i));
-        }
-        System.out.println();
-        Vector<Integer> v23 = new Vector<Integer> ();
-        v23=vectorHelper.trierVect(v13);
-        for(int i=0;i<=99;i++){
-            System.out.print("   "+v23.get(i));
-        }
     }
 }
